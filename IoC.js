efda4228c8ba4bfee33b7f9b7a7ee2f437348551
RDO16 <91622126+RDO16@users.noreply.github.com>
2021-12-22 15:33:46 +0300

    Add files via upload

---
+++ IoC.js
@@ -0,0 +1,27 @@
+
+// Регистрация пользователя в интернет магазине
+
+const {CurrentAvatar, CurrentPromo} = require("./IoC_server.js");
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.promo = new CurrentPromo();
+        this.avatar = new CurrentAvatar();
+    }
+    sendEmailPromo() {
+        console.log(`Уважаемый покупатель!
+        На этой неделе для вас действует персональная акция!
+        ${this.promo.weekly.product} со скидкой ${this.promo.weekly.per} по цене ${this.promo.weekly.amount}.
+        Спешите преобрести!`)
+    }
+    getAvatar() {
+        console.log(`Ссылка на аватарку пользователя ${this.name} (${this.age} лет): ${this.avatar.href}`)
+    }
+}
+
+
+const user = new User('Вася', 17);
+user.sendEmailPromo();
+user.getAvatar();
